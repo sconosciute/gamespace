81febb6f40569b110bcaeb51f84f8b18b5ad8198
Zac <andersenzb@gmail.com>
2024-02-04 19:05:15 -0800

    Reworked to match new inherited constructor.

--- src/Model/Character.cs
+++ src/Model/Character.cs
@@ -12,14 +12,16 @@ public abstract class Character : Entity
     private int _baseDmg;
     private List<Ability> _abilities;
     
-    public Character(int moveSpeed, RenderObject sprite, int x, int y, int width, int height, bool canCollide,
-        bool canMove, int hp, int maxHp, int energy, int maxEnergy, int baseDmg) : base(moveSpeed, sprite, x, y, width, height,canCollide,  canMove)
+    public Character(int moveSpeed, RenderObject sprite, int x, int y, int width, int height, bool hasCollision,
+        bool hasMovement, int hp, int maxHp, int energy, int maxEnergy, int baseDmg, World world) : 
+        base(moveSpeed, sprite, x, y, width, height,hasCollision, world)
     {
         _hp = hp;
         _maxHp = maxHp;
         _energy = energy;
         _maxEnergy = maxEnergy;
         _baseDmg = baseDmg;
+        //TODO: Simplify constructor and initialize with defaults.
     }
 
     public void Harm(int dmg)
