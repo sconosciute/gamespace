afa17a047b559e03c51419f2ea85a4e345e9558a
Zac <andersenzb@gmail.com>
2024-02-04 19:07:24 -0800

    Reworked to make function with new base constructor.

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -8,14 +8,16 @@ namespace gamespace.Model;
 
 //Side note, we should probably follow a singleton principle with player either here or in main right?
 // like this? public static Player Instance = new Player(); ~ Logan
-public class Player : Character {
-    
-    private string _name;   
-    private string _playerClass;  
-    
-    public Player(string name, string playerClass, int moveSpeed, RenderObject sprite, int x, int y, int width, int height, bool canCollide,
-        bool canMove, int hp, int maxHp, int energy, int maxEnergy, int baseDmg) : base(moveSpeed, sprite,  x, y,  width,  height,  canCollide,
-         canMove, hp, maxHp, energy, maxEnergy, baseDmg)
+public class Player : Character
+{
+    private string _name;
+    private string _playerClass;
+
+    public Player(string name, string playerClass, int moveSpeed, RenderObject sprite, int x, int y, int width,
+        int height, bool hasCollision, bool hasMovement, int hp, int maxHp, int energy, int maxEnergy, int baseDmg,
+        World world)
+        : base(moveSpeed, sprite, x, y, width, height, hasCollision, hasMovement, hp, maxHp, energy, maxEnergy, baseDmg,
+            world)
     {
         _name = name;
         _playerClass = playerClass;
@@ -23,21 +25,21 @@ public class Player : Character {
 
     //inventory
     //special ability
-    
+
     //inventory functions
-    
+
     //special ability function;
-    
-    public override void Move(int newX, int newY)
+
+    public void Move()
     {
-        X += newX;
-        Y += newY;
+        //TODO: Rework this to take in events and adjust movement speed based on input.
     }
 
     public Vector2 ReturnPos()
     {
         return new Vector2(X, Y);
     }
+
     //No longer used here, to avoid breaking MVC conventions. At least not for movement, may remove gameTime
     public override void Update(GameTime gameTime)
     {
