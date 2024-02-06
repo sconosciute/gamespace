1ba3bbb91d7f98aea0ee69d74b7bfccad226f5a0
Zac <andersenzb@gmail.com>
2024-02-06 14:00:56 -0800

    Cleanup, pare down.

--- src/Game1.cs
+++ src/Game1.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Xml.Schema;
 using gamespace.Managers;
 using gamespace.Model;
 using gamespace.View;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace gamespace;
 
 public class Game1 : Game
 {
-    //Use these constants and set _dynamicResolution to false in order to have a static width and height
-    //  Looking at implementing a JSON file to contain these system settings
     private const int DefaultResolutionWidth = 1280;
     private const int DefaultResolutionHeight = 1024;
-
-
-    public Animation AnimationTimer; 
     
     public static int ScreenHeight;
     public static int ScreenWidth;
@@ -25,8 +18,7 @@ public class Game1 : Game
     private readonly GraphicsDeviceManager _graphics;
     private Boolean _isFullScreen = false;
     private Boolean _dynamicResolution = false;
-
-    private SpriteBatch _spriteBatch;
+    
     private Camera _camera;
     private Player _player;
 
@@ -66,10 +58,11 @@ public class Game1 : Game
     {
         //TODO: Add entity, bgProp, fgProp lists to initialize into.
         //TODO: initialize the player into it's own super special variable.
-        World world = new World(200, 200);
+        
         ScreenHeight = _graphics.PreferredBackBufferHeight; //Gets the height of the screen
         ScreenWidth = _graphics.PreferredBackBufferWidth;   //Gets the width of the screen
-        AnimationTimer = new Animation();
+        
+        World world = new World(200, 200);
         _player = new Player("Player", world);
         
         ScreenWidth = _graphics.PreferredBackBufferWidth; //Gets the width of the screen
@@ -79,7 +72,7 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         //TODO: Load content inside render object.
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
         
         _camera = new Camera();
     }
