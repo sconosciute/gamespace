6b4a79832ffe1102ae53c41af19209417bdd2108
Zac <andersenzb@gmail.com>
2024-02-06 13:56:10 -0800

    Pared down Game1.cs
    Removed redundant variables and messy code to rework.

--- src/Game1.cs
+++ src/Game1.cs
@@ -14,12 +14,6 @@ public class Game1 : Game
     //  Looking at implementing a JSON file to contain these system settings
     private const int DefaultResolutionWidth = 1280;
     private const int DefaultResolutionHeight = 1024;
-    //Player control Keys
-    private const Keys ForwardButton = Keys.W;
-    private const Keys LeftButton = Keys.A;
-    private const Keys RightButton = Keys.D;
-    private const Keys DownButton = Keys.S;
-    private const Keys SprintButton = Keys.Space;
 
 
     public Animation AnimationTimer; 
@@ -34,13 +28,6 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private Camera _camera;
     private Player _player;
-    private Texture2D _playerModel;
-    private Texture2D _testingBackGround;
-    private Texture2D _spaceBackground;
-    
-    private RenderObject _renderPlayer;
-    private RenderObject _renderTempBackground;
-    private RenderObject _renderSpaceBackground;
 
     public Game1()
     {
@@ -82,8 +69,7 @@ public class Game1 : Game
         ScreenHeight = _graphics.PreferredBackBufferHeight; //Gets the height of the screen
         ScreenWidth = _graphics.PreferredBackBufferWidth;   //Gets the width of the screen
         AnimationTimer = new Animation();
-        _player = new Player("Player32", "Rogue", 1, _renderPlayer, 0, 0, 32, 32, true,
-            true, 100, 100, 100, 100, 10, world);
+        _player = new Player("Player", world);
         
         ScreenWidth = _graphics.PreferredBackBufferWidth; //Gets the width of the screen
         base.Initialize();
@@ -91,29 +77,21 @@ public class Game1 : Game
 
     protected override void LoadContent()
     {
-        //TODO: move this into renderObject.
+        //TODO: Load content inside render object.
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        _testingBackGround = Content.Load<Texture2D>("checkeredBoardBackGround");
-        _playerModel = Content.Load<Texture2D>("playerCircle32");
-        _spaceBackground = Content.Load<Texture2D>("spaceBackgroundResized");
-        
-        _renderPlayer = new RenderObject(_playerModel, _player.ReturnPos(), 1, 1); //Have to load here to avoid _playerModel being NULL
-        _renderTempBackground = new RenderObject(_testingBackGround, Vector2.Zero, 1, 1); //Same problem
-        _renderSpaceBackground = new RenderObject(_spaceBackground, Vector2.Zero, 1, 1);
         
         _camera = new Camera();
     }
 
     protected override void Update(GameTime gameTime)
     {
+        //TODO: Make work with fixed update step
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
         
         PlayerInput(gameTime);
         
-        _player.Update(gameTime); 
-        _renderPlayer.Position = _player.ReturnPos();
         AnimationTimer.Update(gameTime);
         _camera.centerOn(_player); //Calls this after every update to keep player centered
         base.Update(gameTime);
@@ -124,16 +102,6 @@ public class Game1 : Game
         //TODO: Move render code into renderObject, just call draw method on entity and prop lists.
         GraphicsDevice.Clear(Color.CornflowerBlue);
         
-        _spriteBatch.Begin(transformMatrix: _camera.Transform);
-        Vector2 playerSize = Vector2.One;
-        _spriteBatch.Draw(_testingBackGround, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, Vector2.One,
-            SpriteEffects.None, 1f);
-        _spriteBatch.Draw(_playerModel, _player.ReturnPos(), null, Color.White, 0f, Vector2.Zero, playerSize,
-            SpriteEffects.None, 0f); //Layer 0f background, 1f foreground
-        _renderTempBackground.Draw(_spriteBatch);
-        _renderSpaceBackground.Draw(_spriteBatch);
-        _renderPlayer.Draw(_spriteBatch);
-        _spriteBatch.End();
         base.Draw(gameTime);
     }
 
