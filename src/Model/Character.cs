9c37d2cbfb1f2f229abd71e4c65a233662251b2e
Zac A <andersenzb@gmail.com>
2024-02-07 14:24:08 -0800

    Made Health accessible, removed unused code.

--- src/Model/Character.cs
+++ src/Model/Character.cs
@@ -6,34 +6,19 @@ namespace gamespace.Model;
 
 public abstract class Character : Entity
 {
-    private int _hp;
     private int _maxHp;
     private int _energy;
     private int _maxEnergy;
     private int _baseDmg;
     private List<Ability> _abilities;
 
+    public int Health { get; protected set; }
+
     public Character(Vector2 worldCoordinate, int width, int height, int hp, int energy, int baseDmg, World world) :
         base(width, height, world, worldCoordinate)
     {
-        _hp = _maxHp = hp;
+        Health = _maxHp = hp;
         _energy = _maxEnergy = energy;
         _baseDmg = baseDmg;
-    }
-
-    public void Harm(int dmg)
-    {
-        // Implementation for harming the character
-    }
-
-    public void Heal(int hp)
-    {
-        // Implementation for healing the character
-    }
-
-    public bool UseAbility(Ability ability)
-    {
-        // Implementation for using an ability
-        return false;
     }
 }
