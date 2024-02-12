ecf40b80c2c2d8bdde695a92f4dd31dcc7ec6307
Logan <hawkeyegoat@gmail.com>
2024-02-12 14:58:11 -0800

    Finished JSON launch settings, will default to constants if not already created, can call UpdateSettings to change these later, or change const before initializing settings

--- src/Game1.cs
+++ src/Game1.cs
@@ -9,10 +9,15 @@ namespace gamespace;
 
 public class Game1 : Game
 {
+    private const int DefaultResHeight = 800;
+    private const int DefaultResWidth = 800;
+    private const bool FullScreen = false;
+    private const bool DynamicRes = false;
     private const int UpdateTimeDelta = (1000 / 30);
     private double _lastUpTime;
     private readonly GameManager _gm;
 
+    private RenderObject _playerRender;
 
     public Game1()
     {
@@ -21,8 +26,7 @@ public class Game1 : Game
         var adapter = new GraphicsAdapter();
         
         const string fileName = "launchConfig.json";
-        var path = Path.Combine(Environment.CurrentDirectory, "Configs\\", fileName);
-        var settings = LoadSettings(path);
+        var settings = LoadSettings(fileName);
         if (settings != null)
         {
             if (settings.IsDynamic)
@@ -87,17 +91,30 @@ public class Game1 : Game
         _gm.Draw();
         base.Draw(gameTime);
     }
-    private static LaunchSettings LoadSettings(string path)
+    private static LaunchSettings LoadSettings(string fileName)
     {
         try
         {
-            var jsonString = File.ReadAllText(path);
-            var settings = JsonSerializer.Deserialize<LaunchSettings>(jsonString)!;
+            //var path = Path.Combine(Environment.CurrentDirectory, "Configs\\", fileName);
+            var jsonString = File.ReadAllText(fileName);
+            var settings = JsonSerializer.Deserialize<LaunchSettings>(jsonString);
             return settings;
         }
         catch (DirectoryNotFoundException)
         {
-            return null;
+            LaunchSettings defaultSettings = new LaunchSettings();
+            defaultSettings.DefaultResHeight = DefaultResHeight;
+            defaultSettings.DefaultResWidth = DefaultResWidth;
+            defaultSettings.IsFullScreened = FullScreen;
+            defaultSettings.IsDynamic = DynamicRes;
+            UpdateSettings(fileName, defaultSettings);
+            return defaultSettings;
         }
+    }
+
+    private static void UpdateSettings(string path, Object defaultSettings)
+    {
+        string json = JsonSerializer.Serialize(defaultSettings);
+        File.WriteAllText(path, json);
     }
 }
