65bbdc3619b12db08cd10ddc3f9af7890d462b70
William <whay@uw.edu>
2024-02-16 19:41:00 -0800

    Added test image
    TODO:
    -Need to make image only show the character idling at first instead of the whole sheet

--- src/Textures.cs
+++ src/Textures.cs
@@ -5,6 +5,6 @@ namespace gamespace;
 public struct Textures
 {
     public const string TestTile = "tile";
-    public const string Player = "playerCircle32";
+    public const string Player = "test";
     public const string TestBars = "TestRainbow";
 }
