054dae8fa78697d20da48a38a23ad79787c7b4e7
Logan Atkinson <loganatkinson60@gmail.com>
2024-02-19 21:50:56 -0800

    Outlined a lot more for items, need to make more types, and change it so it heals a character instead of just logging it.

--- src/Model/Item.cs
+++ src/Model/Item.cs
@@ -1,21 +1,44 @@
 using System;
+using Loyc.Syntax;
 
 namespace gamespace.Model;
-
+public delegate void ItemUsedCallback();
 public class Item
 {
     // Figure out later how to draw these items in inventory.
-    private readonly Boolean _canUseOutCombat;
-    private readonly String _itemName;
-    private readonly String _itemDescription;
+    private readonly bool _canUseOutCombat;
+    private readonly string _itemName;
+    private readonly string _itemDescription;
+    private readonly ItemType _type;
+    public enum ItemType
+    {
+        HealthPot = 0
+        
+    }
+    
 
-    public Item(Boolean canUseOutCombat, String itemName, String itemDescription)
+   // public delegate void ItemUsedCallback();
+    public Item(bool canUseOutCombat, string itemName, string itemDescription, ItemType type)
     {
         _canUseOutCombat = canUseOutCombat;
         _itemName = itemName;
         _itemDescription = itemDescription;
+        _type = type;
     }
-    public delegate void ItemUsedCallback(); 
+
+    public ItemUsedCallback GetItemUse()
+    {
+        switch (_type)
+        {
+            case 0 :
+                void HealPotionUse() => Console.Write("You have been healed!");
+                return HealPotionUse;
+            
+            default:
+                void NoReference() => Console.Write("Cannot find case");
+                return NoReference;
+        }
+    }
     
     //Properties
     public String ItemName
