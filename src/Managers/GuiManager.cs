e0042789309ad4262ac5e2ac0dc8c308c2528a37
Zac <andersenzb@gmail.com>
2024-02-20 17:30:19 -0800

    Basic scaffold of GUI system.

---
+++ src/Managers/GuiManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using gamespace.View;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace gamespace.Managers;
+
+public class GuiManager
+{
+    private List<GuiPanel> _panels = new();
+
+    public Texture2D OpaqueBg { get; init; }
+    public Texture2D TransparentBg { get; init; }
+    
+}
---
+++ src/View/GuiPanel.cs
@@ -0,0 +1,30 @@
+﻿using gamespace.Managers;
+using Microsoft.Xna.Framework;
+
+namespace gamespace.View;
+
+public abstract class GuiPanel
+{
+    /// <summary>
+    /// Determines if this panel should be shown to the user.
+    /// </summary>
+    public bool Shown { get; set; } = false;
+    
+    /// <summary>
+    /// Determines if this panel should capture input.
+    /// </summary>
+    public bool IsActive { get; set; } = false;
+
+    public GuiManager Manager { get; init; }
+    
+    private string _title;
+    private Rectangle _drawBox;
+
+
+    protected GuiPanel(string title, Rectangle drawBox, GuiManager manager)
+    {
+        _title = title;
+        _drawBox = drawBox;
+        Manager = manager;
+    }
+}
