6016d5dce8c2e6443b22a6f93f6531e4de902216
Zac <andersenzb@gmail.com>
2024-02-20 17:30:02 -0800

    Added Texture entries for new backgrounds.

--- src/Textures.cs
+++ src/Textures.cs
@@ -8,4 +8,6 @@ public struct Textures
     public const string Player = "testDemon";
     public const string TestBars = "TestRainbow";
     public const string Collider = "ColliderTile";
+    public const string TransparentBg = "transparent";
+    public const string OpaqueBg = "opaque";
 }
