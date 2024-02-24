047468253425a790cc2befd35971c1988cbd0a42
Zac <andersenzb@gmail.com>
2024-02-24 11:45:41 -0800

    Removed dummy menu popup

--- gamespace/src/Managers/GameManager.cs
+++ gamespace/src/Managers/GameManager.cs
@@ -64,8 +64,7 @@ public class GameManager
         _world.TryPlaceTile(new Point(20, 5), BuildTile(new Vector2(20f, 5f), Build.Props.Wall));
         _world.TryPlaceTile(new Point(20, 6), BuildTile(new Vector2(20f, 6f), Build.Props.Wall));
         _world.TryPlaceTile(new Point(21, 5), BuildTile(new Vector2(21f, 5f), Build.Props.Wall));
-
-        _gui.OpenMainMenu();
+        
     }
 
     //=== GAME LOOP ===-------------------------------------------------------------------------------------------------
