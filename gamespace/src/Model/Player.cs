ba4c92c8c65812bfcd1d4d2008dd588c456c15f5
Logan <Hawkeyegoat@gmail.com>
2024-02-26 15:00:28 -0800

    Added documentation for Player

--- gamespace/src/Model/Player.cs
+++ gamespace/src/Model/Player.cs
@@ -3,19 +3,51 @@ using gamespace.Managers;
 using Microsoft.Xna.Framework;
 
 namespace gamespace.Model;
-
+/// <summary>
+/// A class to generate and store our player.
+/// </summary>
 public class Player : Character
 {
+    /// <summary>
+    /// A constant to provide the size of our players usable inventory.
+    /// </summary>
     private const int InventorySize = 5;
+    
+    /// <summary>
+    /// A constant to provide the size of our players key item inventory.
+    /// </summary>
     private const int KeyInventorySize = 4;
 
+    /// <summary>
+    /// A string to store the name of our player.
+    /// </summary>
     private string _name;
+    
+    /// <summary>
+    /// An array to store our players usable items.
+    /// </summary>
     private Item[] _inventory;
+    
+    /// <summary>
+    /// An array to store our players key items.
+    /// </summary>
     private Item[] _keyItemInventory;
-
+    
+    /// <summary>
+    /// A public property to access our players usable inventory.
+    /// </summary>
     public Item[] Inventory => _inventory;
+    
+    /// <summary>
+    /// A public property to access our players key inventory.
+    /// </summary>
     public Item[] KeyItemInventory => _keyItemInventory;
 
+    /// <summary>
+    /// A constructor to build our player.
+    /// </summary>
+    /// <param name="name"> The name our player should have. </param>
+    /// <param name="world"> The world our player should be put into. </param>
     public Player(string name, World world)
         : base(Vector2.Zero, 1, 1, 100, 100, 10, world)
     {
@@ -23,7 +55,8 @@ public class Player : Character
         _inventory = new Item[InventorySize];
         _keyItemInventory = new Item[KeyInventorySize];
     }
-
+    
+    //TODO: Add documentation for this method.
     public new void FixedUpdate()
     {
         var direction = InputManager.Direction;
@@ -31,10 +64,15 @@ public class Player : Character
 
         base.FixedUpdate();
     }
-
+    
+    /// <summary>
+    /// Uses the item at a given index of our players usable inventory.
+    /// </summary>
+    /// <param name="inventorySlot"> The index our player wishes to use. </param>
+    /// <returns> False if the given index is out of bounds, or does not contain an item. True if the item is used</returns>
     public bool InventoryUse(int inventorySlot)
     {
-        if (inventorySlot is < 0 or > InventorySize)
+        if (inventorySlot is < 0 or >= InventorySize)
         {
             return false;
         }
@@ -50,7 +88,12 @@ public class Player : Character
         wantedItemUse.Invoke();
         return true;
     }
-
+    
+    /// <summary>
+    /// Adds an item to the first open slot of Key inventory if it is a key item, or to the first slot of usable inventory.
+    /// </summary>
+    /// <param name="newItem"> The item to place in our players inventory. </param>
+    /// <returns> Returns false if the players inventory is full, true if there is an open slot. </returns>
     public bool AddToInventory(Item newItem)
     {
         if (newItem.IsKeyItem)
@@ -78,7 +121,11 @@ public class Player : Character
             return true;
         }
     }
-
+    
+    /// <summary>
+    /// Provides a string representation of our player.
+    /// </summary>
+    /// <returns> A string representation of player. </returns>
     public override string ToString()
     {
         var result = base.ToString() + " Name: " + _name;
