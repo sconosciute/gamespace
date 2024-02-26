c80a75862d8d4a9ddfe573ccdbe06cac30f253c4
Logan <Hawkeyegoat@gmail.com>
2024-02-26 13:36:42 -0800

    Added build methods for key items

--- gamespace/src/Managers/Build.cs
+++ gamespace/src/Managers/Build.cs
@@ -84,7 +84,33 @@ public static class Build
             largePotion.UseLargePotion();
             return largePotion;
         }
+        //Key Items
 
+        public static Item Cog()
+        {
+            var cog = new Item("Cog", "This will help you fix the escape pod", true, Item.ItemType.KeyItem);
+            return cog;
+        }
+
+        public static Item Wires()
+        {
+            var wires = new Item("Wires", "This will help you fix the escape pod", true, Item.ItemType.KeyItem);
+            return wires;
+        }
+
+        public static Item Lever()
+        {
+            var lever = new Item("Lever", "This will help you fix the escape pod", true, Item.ItemType.KeyItem);
+            return lever;
+        }
+
+        public static Item ControlPanel()
+        {
+            var controlPanel = new Item("Control panel", "This will help you fix the escape pod", true, Item.ItemType.KeyItem);
+            return controlPanel;
+        }
+        
+        //Test item, this can be deleted upon final iteration.
         public static Item TestItem()
         {
             var testItem = new Item("Test",
