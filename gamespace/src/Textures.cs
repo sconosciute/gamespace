d0694a1ce2446234bb417b569221a3c2efa7fc88
William <whay@uw.edu>
2024-03-11 02:12:42 -0700

    I need more bullets!

--- gamespace/src/Textures.cs
+++ gamespace/src/Textures.cs
@@ -19,5 +19,5 @@ public struct Textures
     public const string Spike = "spike";
     public const string DarkSquareWall = "darkSquareWall";
     public const string DarkDiamondWall = "darkDiamondWall";
-    public const string Bullet = "RedCircle16";
+    public const string Bullet = "bullet";
 }
