cb0ddaf8f01c6afc6330f93c86a74710615e70ca
Zac <andersenzb@gmail.com>
2024-03-13 10:57:49 -0700

    Technically should be a database now. Not tested! ¯\_(ツ)_/¯

---
+++ gamespace/src/Managers/Database/DbHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using SQLite;
+
+namespace gamespace.Managers.Database;
+
+public static class DbHandler
+{
+    private static readonly string ConnPath =
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Gamespace", FilePath);
+
+    private const string FilePath = "stats.db";
+    private static readonly ILogger Log = Globals.LogFactory.CreateLogger<Game1>();
+
+    private static bool TryOpen(out SQLiteConnection db)
+    {
+        try
+        {
+            db = new SQLiteConnection(ConnPath);
+            db.CreateTable<Statistic>();
+            return true;
+        }
+        catch (Exception e)
+        {
+            db = null;
+            Log.LogError("Failed to establish database connection due to {e}", e);
+            return false;
+        }
+    }
+
+    public static void WriteStat(Statistic.Stats statName, int value)
+    {
+        if (!TryOpen(out var db)) return;
+
+        var stat = new Statistic()
+        {
+            StatName = statName.ToString(),
+            Value = value
+        };
+
+        db.Update(stat);
+        db.Close();
+    }
+}
---
+++ gamespace/src/Managers/Database/Statistic.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+
+namespace gamespace.Managers.Database;
+
+[Table("Statistic")]
+public class Statistic
+{
+    [PrimaryKey]
+    [Column("statName")]
+    public string StatName { get; set; }
+    
+    [Column("value")]
+    public int Value { get; set; }
+
+    /// <summary>
+    /// IDs for stat columns.
+    /// </summary>
+    public enum Stats
+    {
+        TimeInDungeon,
+        DeathCount,
+        WinCount,
+        MobKillCount,
+        ItemsFound
+    }
+    
+}
