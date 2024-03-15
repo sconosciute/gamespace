4173d27b63bac54a45e261e7513101693bfdc7e8
Logan Atkinson <loganatkinson60@gmail.com>
2024-03-15 02:45:09 -0700

    Outlined Tile Test.

---
+++ gamespaceunittests/TileTest.cs
@@ -0,0 +1,17 @@
+﻿using gamespace.Managers;
+using gamespace.Model;
+using Microsoft.Xna.Framework;
+
+namespace gamespaceunittests;
+
+public class TileTest
+{
+    //This feels like overkill
+    [Test]
+    public void TestTileConstructor()
+    {
+        var prop = new Prop(Vector2.Zero, 1, 1, false);
+        var tile = new Tile(prop);
+        Assert.That(tile.Prop, Is.EqualTo(prop));
+    }
+}
